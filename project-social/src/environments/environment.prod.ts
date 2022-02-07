41f8fddbbbe68391b7e3cd15811a43f09f02b60d
Pietro Scaduto <pietro.scaduto25@gmail.com>
2022-02-07 10:51:01 +0100

    feat: homepage base

--- project-social/src/environments/environment.prod.ts
+++ project-social/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDCzkvuSaNKWCG8cI1IbZtodg910rDVVyA',
+    authDomain: 'mysocialmediaproject-9fdde.firebaseapp.com',
+    projectId: 'mysocialmediaproject-9fdde',
+    storageBucket: 'mysocialmediaproject-9fdde.appspot.com',
+    messagingSenderId: '316104900805',
+    appId: '1:316104900805:web:f143e32dc677d43395f0ac',
+    measurementId: 'G-T4R7PQ7WFR',
+  },
 };
--- project-social/src/environments/environment.ts
+++ project-social/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebaseConfig: {
+    apiKey: 'AIzaSyDCzkvuSaNKWCG8cI1IbZtodg910rDVVyA',
+    authDomain: 'mysocialmediaproject-9fdde.firebaseapp.com',
+    projectId: 'mysocialmediaproject-9fdde',
+    storageBucket: 'mysocialmediaproject-9fdde.appspot.com',
+    messagingSenderId: '316104900805',
+    appId: '1:316104900805:web:f143e32dc677d43395f0ac',
+    measurementId: 'G-T4R7PQ7WFR',
+  },
 };
 
 /*
